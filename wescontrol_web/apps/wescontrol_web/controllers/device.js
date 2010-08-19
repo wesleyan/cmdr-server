6ea5375c10308b97cda14f7566c8b5c22167e9c0
Micah Wylde <mwylde@wesleyan.edu>
2010-08-19 15:23:15 -0700

    Rearranging things in preparation for splitting the repository into three (devices, roomtrol and roomtrol-server)

---
+++ wescontrol_web/apps/wescontrol_web/controllers/device.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   WescontrolWeb.deviceController
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals WescontrolWeb */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+WescontrolWeb.deviceController = SC.ObjectController.create(
+/** @scope WescontrolWeb.deviceController.prototype */ {
+
+	contentBinding: SC.Binding.single('WescontrolWeb.roomController.selection')
+	
+}) ;
