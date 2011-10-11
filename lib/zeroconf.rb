8490f3482d48311c6250fb66506ee75588dc8199
mwylde <mwylde@wesleyan.edu>
2011-10-11 16:30:13 -0400

    Fixed previous commit

--- lib/zeroconf.rb
+++ lib/zeroconf.rb
@@ -180,8 +180,8 @@ module Wescontrol
               # TODO: implement a port finder
               # try a different port and retry
               
-            rescue OpenSSL::PKey::RSAError
-              DaemonKit.logger.error "Failed to unlock private key"
+            rescue
+              DaemonKit.logger.error "Problem with SSH: #{$!}"
             end
           end
         end
