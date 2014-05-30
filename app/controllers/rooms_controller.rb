9269b0c1227e4729d0b34b2857fd739500767798
Brian Gapinski <bgapinski@wesleyan.edu>
2014-05-30 18:19:16 -0400

    Can update documents

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -41,7 +41,7 @@ class RoomsController < ApplicationController
   # PATCH/PUT /rooms/1.json
   def update
     respond_to do |format|
-      if @room.update(room_params)
+      if @room.update_attributes(room_params)
         format.html { redirect_to @room, notice: 'Room was successfully updated.' }
         format.json { render :show, status: :ok, location: @room }
       else
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,28 @@
 class Room < CouchRest::Model::Base
   use_database 'rooms'
 
-  property :belongs_to, String
-  property :name, String
-  property :attrs, Hash
+  property :belongs_to, String, default: ''
+  property :name, String, default: 'Unnamed'
+  property :hostname, String, default: 'localhost'
+  property :attrs, default: {} do
+    property :name, String, default: 'Unnamed'
+    property :hostname, String, default: 'localhost'
+    property :mac, String, default: ''
+    property :projector, String, default: ''
+    property :volume, String, default: ''
+    property :switcher, String, default: ''
+    property :ir_switcher, String, default: ''
+    property :blurayplayer, String, default: ''
+    property :computer, String, default: ''
+    property :ports, [String], default: []
+  end
 
   design do
     view :by_name
   end
+
+  design do
+    view :by_hostname
+  end
+
 end
