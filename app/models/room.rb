23c2be30c0c99c66324563d1c3d2264710066108
Brian Gapinski <bgapinski@wesleyan.edu>
2014-05-30 12:41:22 -0400

    Update display name of rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,15 +1,16 @@
 class Room
   include ActiveModel::Conversion
-  extend  ActiveModel::Naming
+  extend ActiveModel::Naming
 
-  @db = CouchRest.database("http://localhost:5984/rooms")
+  @db = CouchRest.database('http://localhost:5984/rooms')
 
   def self.all
-    return @db.get("_design/cmdr_web").view("rooms")["rows"]
+    @db.get('_design/cmdr_web').view('rooms')['rows']
+      .map { |doc| doc['value'] }
   end
 
-  def self.find id
-    return @db.get(id).to_hash
+  def self.find(id)
+    @db.get(id).to_hash
   end
 
   def persisted?
